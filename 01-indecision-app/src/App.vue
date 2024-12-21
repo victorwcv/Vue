901f48ce3a16c262bab4e48b16ace771ca8c1101
William <victorwccv@gmail.com>
2024-12-21 01:15:42 -0500

    feat: states and events

--- 01-indecision-app/src/App.vue
+++ 01-indecision-app/src/App.vue
@@ -1,10 +1,35 @@
 <template>
-  <h1>Hola Vue</h1>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ counterSquare }}</h3>
+
+    <div>
+      <button @click="decrement">-1</button>
+      <button @click="reset">Reset</button>
+      <button @click="increment">+1</button>
+    </div>
+  </section>
 </template>
 
 
 <script lang="ts" setup>
-console.log('hola')
+import { ref, computed } from 'vue';
+
+
+const counter = ref(0);
+const counterSquare = computed(() => counter.value ** 2);
+const increment = () => {
+  counter.value++;
+};
+const decrement = () => {
+  if (counter.value === 0) return;
+  counter.value--;
+}
+
+const reset = () => counter.value = 0;
+
+
+
 </script>
 
 
