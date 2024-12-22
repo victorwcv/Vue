2c68f11e40e4e0c3c395eda63fdafcc95e02f422
William <victorwccv@gmail.com>
2024-12-21 19:31:16 -0500

    feat: traditional component

--- 01-indecision-app/src/App.vue
+++ 01-indecision-app/src/App.vue
@@ -2,10 +2,12 @@
   <h1>Hello Vue</h1>
   <hr>
   <MyCounter :value="10" />
+  <MyCounterScript :value="3" />
 </template>
 
 <script lang="ts" setup>
 import MyCounter from './components/MyCounter.vue';
+import MyCounterScript from './components/MyCounterScript.vue';
 
 
 </script>
