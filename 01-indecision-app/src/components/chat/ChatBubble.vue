d023e8eea45807f5e478d745ac05e28bf00487eb
William <victorwccv@gmail.com>
2024-12-21 21:02:35 -0500

    feat: message bubble design

---
+++ 01-indecision-app/src/components/chat/ChatBubble.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <!-- Example Message -->
+  <div v-if="itsMine" class="flex justify-end">
+    <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
+      {{ message }}
+    </div>
+  </div>
+
+  <!-- Example Received Message -->
+  <div v-else class="flex">
+    <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
+      <span class="capitalize">{{ message }}</span>
+      <img v-if="image" :src="image" alt="Yes No Image" class="w-52 h-52 object-cover rounded-md">
+    </div>
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+
+interface Props {
+  message: string;
+  itsMine: boolean;
+  image?: string;
+}
+
+defineProps<Props>();
+
+</script>
--- 01-indecision-app/src/components/chat/ChatMessages.vue
+++ 01-indecision-app/src/components/chat/ChatMessages.vue
@@ -3,20 +3,15 @@
   <div class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
-      <!-- Example Message -->
-      <div class="flex justify-end">
-        <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-          Hey, how's your day going?
-        </div>
-      </div>
+      <ChatBubble :its-mine="true" :message="'Hi, how are you?'" />
+      <ChatBubble :its-mine="false" :message="'Good, how about you?'"
+        image="https://yesno.wtf/assets/yes/3-422e51268d64d78241720a7de52fe121.gif" />
 
-      <!-- Example Received Message -->
-      <div class="flex">
-        <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-          Not too bad, just a bit busy. How about you?
-        </div>
-      </div>
     </div>
   </div>
 
 </template>
+
+<script setup lang="ts">
+import ChatBubble from './ChatBubble.vue';
+</script>
