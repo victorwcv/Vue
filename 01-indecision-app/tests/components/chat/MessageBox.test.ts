e611874f6ebe8bf104b83a649c04e5a7363e6be0
William <victorwccv@gmail.com>
2024-12-23 11:43:07 -0500

    test: testing emits

--- 01-indecision-app/tests/components/chat/MessageBox.test.ts
+++ 01-indecision-app/tests/components/chat/MessageBox.test.ts
@@ -2,10 +2,20 @@ import MessageBox from '@/components/chat/MessageBox.vue';
 import { mount } from '@vue/test-utils';
 
 describe('Testing <MessageBox/>', () => {
+  const wrapper = mount(MessageBox);
+
   it('should render input and button correctly', () => {
-    const wrapper = mount(MessageBox);
     expect(wrapper.html()).toMatchSnapshot();
     expect(wrapper.find('input[type="text"]').exists()).toBe(true);
     expect(wrapper.find('button').exists()).toBe(true);
+  });
+
+  it('should emits sendMessage event when the message is sent', async () => {
+    const message = 'Hola Mundo';
+
+    await wrapper.find('input[type="text"]').setValue(message);
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted('sendMessage')?.[0]).toEqual([message]);
   });
 });
