21e9a8a3dcf52c308039943b6a4dffa25758c08d
William <victorwccv@gmail.com>
2024-12-26 12:44:29 -0500

    feat: estructura html

--- 03-vue-spa/src/modules/landing/pages/HomePage.vue
+++ 03-vue-spa/src/modules/landing/pages/HomePage.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-    <h1>Hello Home!</h1>
+  <div class="text-center">
+    <h1 class="text-4xl font-bold tracking-tighter sm:text-5xl">
+      Bienvenido a nuestro sitio web
+    </h1>
+    <p class="mx-auto max-w-[600px] text-gray-500 md:text-xl">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
   </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
