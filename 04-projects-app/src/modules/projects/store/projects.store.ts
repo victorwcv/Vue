0b01cfe893d0a2ff8c8ddcbfc58d31ab9c415808
William <victorwccv@gmail.com>
2025-01-05 22:02:04 -0500

    feat: persist data in localStore

--- 04-projects-app/src/modules/projects/store/projects.store.ts
+++ 04-projects-app/src/modules/projects/store/projects.store.ts
@@ -1,28 +1,11 @@
 import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
 import type { Project } from '../types/project.interface';
+import { useLocalStorage } from '@vueuse/core';
 
-const initialState: Project[] = [
-  {
-    id: "1",
-    name: 'Project 1',
-    tasks: [
-      { id: 1, name: 'Task 1', completedAt: null },
-      { id: 2, name: 'Task 2', completedAt: null },
-    ],
-  },
-  {
-    id: "2",
-    name: 'Project 2',
-    tasks: [
-      { id: 3, name: 'Task 3', completedAt: null },
-      { id: 4, name: 'Task 4', completedAt: null },
-    ],
-  },
-];
 
 export const useProjectsStore = defineStore('projects', () => {
-  const projects = ref<Project[]>([...initialState]);
+  const projects = ref(useLocalStorage<Project[]>('projects', []));
 
   const addProject = (name: string) => {
     if (name.trim() === '') return;
