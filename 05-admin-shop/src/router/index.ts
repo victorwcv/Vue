d285a0cb736feb58d68ff60cbdc052a63959d94d
William <victorwccv@gmail.com>
2025-01-07 19:44:03 -0500

    05-admin-shop: project created and install tailwind

---
+++ 05-admin-shop/src/router/index.ts
@@ -0,0 +1,15 @@
+import ShopLayout from '@/modules/shop/layouts/ShopLayout.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'shop',
+      component: ShopLayout,
+    },
+  ],
+});
+
+export default router;
