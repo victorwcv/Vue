ad4421ae3f99bae4192e959b5fb485d7101393e7
William <victorwccv@gmail.com>
2025-01-09 12:47:11 -0500

    feat: tanStack Query added

--- 05-admin-shop/src/App.vue
+++ 05-admin-shop/src/App.vue
@@ -1,3 +1,8 @@
 <template>
   <RouterView />
+  <VueQueryDevtools />
 </template>
+
+<script lang="ts" setup>
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
+</script>
--- 05-admin-shop/src/main.ts
+++ 05-admin-shop/src/main.ts
@@ -2,13 +2,16 @@ import './main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
+app.use(VueQueryPlugin);
 
 app.mount('#app');
