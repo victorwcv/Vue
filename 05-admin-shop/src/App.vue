f15f6c42f929a217905daa5f56e9e5c71b071a99
William <victorwccv@gmail.com>
2025-01-15 14:28:04 -0500

    feat: fullscreen loader for is checking status

--- 05-admin-shop/src/App.vue
+++ 05-admin-shop/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <RouterView />
+  <FullscreenLoader v-if="authStore.isChecking" />
+  <RouterView v-else />
   <VueQueryDevtools />
 </template>
 
@@ -8,27 +9,27 @@ import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
 import { useAuthStore } from './modules/auth/stores/auth.store';
 import { AuthStatus } from './modules/auth/types';
 import { useRoute, useRouter } from 'vue-router';
+import FullscreenLoader from './modules/common/components/FullscreenLoader.vue';
 
 const authStore = useAuthStore();
 
 const router = useRouter();
 const route = useRoute();
 
-authStore.$subscribe((_, state) => {
-  if( state.authStatus === AuthStatus.checking ) {
-    authStore.checkAuthStatus();
-    return;
-  }
+authStore.$subscribe(
+  (_, state) => {
+    if (state.authStatus === AuthStatus.checking) {
+      authStore.checkAuthStatus();
+      return;
+    }
 
-  if (route.path.includes('/auth') && state.authStatus === AuthStatus.authenticated) {
-    router.replace({ name: 'home' });
-    return;
-  }
-
-
-
-
-},{
-  immediate: true
-});
+    if (route.path.includes('/auth') && state.authStatus === AuthStatus.authenticated) {
+      router.replace({ name: 'home' });
+      return;
+    }
+  },
+  {
+    immediate: true,
+  },
+);
 </script>
